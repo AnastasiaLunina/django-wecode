7121cf9bf97734f235f28f4046c3240271fec45d
Anastasia Lunina <lunina.anastasiya@gmail.com>
2022-11-16 10:58:12 -0500

    Add single user section

--- users/views.py
+++ users/views.py
@@ -10,5 +10,13 @@ def profiles(request):
     return render(request, 'users/profiles.html', context)
 
 def user_profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    skills_with_description = profile.skill_set.exclude(description__exact="")
+    skills_without_description = profile.skill_set.filter(description="")
 
-    return render(request, 'users/user-profile.html')
+    context = {
+        'profile': profile,
+        'skills_with_description': skills_with_description,
+        'skills_without_description': skills_without_description,
+    }
+    return render(request, 'users/user-profile.html', context)
